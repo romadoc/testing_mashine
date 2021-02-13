15eb7063de59f3f7d402789b8d2f61371cc93035
Roman Doroshenko <coachandconsult3@gmail.com>
2021-02-13 19:22:22 +0300

    file CorrectAnswerLoader is created - allows to get correct answer of THE question of the current test

--- src/main/java/questionloader/QuestionLoader.java
+++ src/main/java/questionloader/QuestionLoader.java
@@ -7,7 +7,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * загрузка из файла вопросов, подвопросов, корректных ответов
+ * загрузка из файла вопросов, подвопросов
  */
 public class QuestionLoader {
 
